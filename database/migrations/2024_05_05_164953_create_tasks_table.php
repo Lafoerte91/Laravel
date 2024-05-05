6c69cdbc65d1cb77db5cd8e1411ef38387e0c6b3
Lafoerte91 <lafoerte@gmail.com>
2024-05-05 19:57:51 +0300

    исроавил ошибку с двойным таймстампом при добавлении полей

--- database/migrations/2024_05_05_164953_create_tasks_table.php
+++ database/migrations/2024_05_05_164953_create_tasks_table.php
@@ -12,11 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
+            $table->id()->primary();
             $table->timestamps();
             $table->string('name');
             $table->boolean('completed')->default(false);
-            $table->timestamps();
         });
     }
 
